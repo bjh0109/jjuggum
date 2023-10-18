9c0667a95a06e0348586eab00c1169ef94218bc2
shofjablas <shluxnsal01@gmail.com>
2023-10-18 21:38:14 +0900

    Revert "큰솔창 출력시 한글 깨짐 수정 및 sample.c 주석 한글 깨짐 수정"
    This reverts commit b5ecf36a1a022e893bf301b90b6a15fa6c91f79e.

--- DD/sample.c
+++ DD/sample.c
@@ -13,13 +13,13 @@ void move_manual(key_t key);
 void move_random(int i, int dir);
 void move_tail(int i, int nx, int ny);
 
-int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // °¢ ÇÃ·¹ÀÌ¾î À§Ä¡, ÀÌµ¿ ÁÖ±â
+int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // ê° í”Œë ˆì´ì–´ ìœ„ì¹˜, ì´ë™ ì£¼ê¸°
 
 void sample_init(void) {
     map_init(15, 40);
     int x, y;
     for (int i = 0; i < n_player; i++) {
-        // °°Àº ÀÚ¸®°¡ ³ª¿À¸é ´Ù½Ã »ı¼º
+        // ê°™ì€ ìë¦¬ê°€ ë‚˜ì˜¤ë©´ ë‹¤ì‹œ ìƒì„±
         do {
             x = randint(1, N_ROW - 2);
             y = randint(1, N_COL - 2);
@@ -35,11 +35,11 @@ void sample_init(void) {
 }
 
 void move_manual(key_t key) {
-    // °¢ ¹æÇâÀ¸·Î ¿òÁ÷ÀÏ ¶§ x, y°ª delta
+    // ê° ë°©í–¥ìœ¼ë¡œ ì›€ì§ì¼ ë•Œ x, yê°’ delta
     static int dx[4] = { -1, 1, 0, 0 };
     static int dy[4] = { 0, 0, -1, 1 };
 
-    int dir;  // ¿òÁ÷ÀÏ ¹æÇâ(0~3)
+    int dir;  // ì›€ì§ì¼ ë°©í–¥(0~3)
     switch (key) {
     case K_UP: dir = DIR_UP; break;
     case K_DOWN: dir = DIR_DOWN; break;
@@ -48,7 +48,7 @@ void move_manual(key_t key) {
     default: return;
     }
 
-    // ¿òÁ÷¿©¼­ ³õÀÏ ÀÚ¸®
+    // ì›€ì§ì—¬ì„œ ë†“ì¼ ìë¦¬
     int nx, ny;
     nx = px[0] + dx[dir];
     ny = py[0] + dy[dir];
@@ -59,12 +59,12 @@ void move_manual(key_t key) {
     move_tail(0, nx, ny);
 }
 
-// 0 <= dir < 4°¡ ¾Æ´Ï¸é ·£´ı
+// 0 <= dir < 4ê°€ ì•„ë‹ˆë©´ ëœë¤
 void move_random(int player, int dir) {
-    int p = player;  // ÀÌ¸§ÀÌ ±æ¾î¼­...
-    int nx, ny;  // ¿òÁ÷¿©¼­ ´ÙÀ½¿¡ ³õÀÏ ÀÚ¸®
+    int p = player;  // ì´ë¦„ì´ ê¸¸ì–´ì„œ...
+    int nx, ny;  // ì›€ì§ì—¬ì„œ ë‹¤ìŒì— ë†“ì¼ ìë¦¬
 
-    // ¿òÁ÷ÀÏ °ø°£ÀÌ ¾ø´Â °æ¿ì´Â ¾ø´Ù°í °¡Á¤(¹«ÇÑ ·çÇÁ¿¡ ºüÁü)
+    // ì›€ì§ì¼ ê³µê°„ì´ ì—†ëŠ” ê²½ìš°ëŠ” ì—†ë‹¤ê³  ê°€ì •(ë¬´í•œ ë£¨í”„ì— ë¹ ì§)    
 
     do {
         nx = px[p] + randint(-1, 1);
@@ -74,9 +74,9 @@ void move_random(int player, int dir) {
     move_tail(p, nx, ny);
 }
 
-// back_buf[][]¿¡ ±â·Ï
+// back_buf[][]ì— ê¸°ë¡
 void move_tail(int player, int nx, int ny) {
-    int p = player;  // ÀÌ¸§ÀÌ ±æ¾î¼­...
+    int p = player;  // ì´ë¦„ì´ ê¸¸ì–´ì„œ...
     back_buf[nx][ny] = back_buf[px[p]][py[p]];
     back_buf[px[p]][py[p]] = ' ';
     px[p] = nx;
@@ -89,7 +89,7 @@ void sample(void) {
     system("cls");
     display();
     while (1) {
-        // player 0¸¸ ¼ÕÀ¸·Î ¿òÁ÷ÀÓ(4¹æÇâ)
+        // player 0ë§Œ ì†ìœ¼ë¡œ ì›€ì§ì„(4ë°©í–¥)
         key_t key = get_key();
         if (key == K_QUIT) {
             break;
@@ -104,7 +104,7 @@ void sample(void) {
             }
         }
 
-        // tick °ªÀÌ 500ÀÇ ¹è¼öÀÏ ¶§¸¸ READY ¸Ş½ÃÁö Ãâ·Â
+        // tick ê°’ì´ 500ì˜ ë°°ìˆ˜ì¼ ë•Œë§Œ READY ë©”ì‹œì§€ ì¶œë ¥
         if (tick == 500) {
             dialog("READY");
             Sleep(1000);
