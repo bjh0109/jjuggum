0992d23e919fff65576d987e939f27c64c714fb2
ohj75992660 <146902060+ohj75992660@users.noreply.github.com>
2023-10-19 22:34:02 +0900

    Update sample.c

--- DD/sample.c
+++ DD/sample.c
@@ -15,14 +15,14 @@ void move_random_1(int i, int dir);
 void move_tail_1(int i, int nx, int ny);
 void print_mugung(void);
 
-int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  // ê°??Œë ˆ?´ì–´ ?„ì¹˜, ?´ë™ ì£¼ê¸°
+int px[PLAYER_MAX], py[PLAYER_MAX], period[PLAYER_MAX];  
 
-//Ã³À½ ½ÃÀÛ
+
 void sample_init_1(void) {
 	map_init(14, 40);
 	int x, y;
 	for (int i = 0; i < n_player; i++) {
-		// °°Àº ÀÚ¸®°¡ ³ª¿À¸é ´Ù½Ã »ý¼º
+		
 		do {
 			x = randint(2, N_ROW - 3);
 			y = randint(38, N_COL - 2);
@@ -34,7 +34,7 @@ void sample_init_1(void) {
 		back_buf[px[i]][py[i]] = '0' + i;  // (0 .. n_player-1)
 	}
 
-	//°úÁ¦ 2¿¡¼­ ÀÌ°Íµµ º¯¼ö·Î ¹Þ°Ô ¹Ù²ã¾ßÇÔ(¿µÈñ)
+
 	back_buf[5][1] = '#';
 	back_buf[6][1] = '#';
 	back_buf[7][1] = '#';
@@ -45,13 +45,13 @@ void sample_init_1(void) {
 	tick = 0;
 }
 
-//0¹ø ÇÃ·¹ÀÌ¾î ¿òÁ÷ÀÌ±â
+
 void move_manual_1(key_t key) {
-	// °¢ ¹æÇâÀ¸·Î ¿òÁ÷ÀÏ ¶§ x, y°ª delta
+	
 	static int dx[4] = { -1, 1, 0, 0 };
 	static int dy[4] = { 0, 0, -1, 1 };
 
-	int dir;  // ¿òÁ÷ÀÏ ¹æÇâ(0~3)
+	int dir;  
 	switch (key) {
 	case K_UP: dir = DIR_UP; break;
 	case K_DOWN: dir = DIR_DOWN; break;
@@ -60,7 +60,7 @@ void move_manual_1(key_t key) {
 	default: return;
 	}
 
-	// ¿òÁ÷¿©¼­ ³õÀÏ ÀÚ¸®
+
 	int nx, ny;
 	nx = px[0] + dx[dir];
 	ny = py[0] + dy[dir];
@@ -71,12 +71,12 @@ void move_manual_1(key_t key) {
 	move_tail_1(0, nx, ny);
 }
 
-//0Á¦¿Ü ÇÃ·¹ÀÌ¾îµé ·£´ýÀÌµ¿
+
 void move_random_1(int player, int dir) {
 	int p = player;  // ÀÌ¸§ÀÌ ±æ¾î¼­...
-	int nx, ny;  // ¿òÁ÷¿©¼­ ´ÙÀ½¿¡ ³õÀÏ ÀÚ¸®
+	int nx, ny;  
 
-	// ¿òÁ÷ÀÏ °ø°£ÀÌ ¾ø´Â °æ¿ì´Â ¾ø´Ù°í °¡Á¤(¹«ÇÑ ·çÇÁ¿¡ ºüÁü)	
+
 	int r = randint(1, 100);
 	gotoxy(N_ROW + 3, 0);
 	printf("%d",r);
@@ -104,9 +104,9 @@ void move_random_1(int player, int dir) {
 }
 
 
-// back_buf[][]¿¡ ±â·Ï
+// back_buf[][]
 void move_tail_1(int player, int nx, int ny) {
-	int p = player;  // ÀÌ¸§ÀÌ ±æ¾î¼­...
+	int p = player; 
 	back_buf[nx][ny] = back_buf[px[p]][py[p]];
 	back_buf[px[p]][py[p]] = ' ';
 	px[p] = nx;
@@ -114,7 +114,7 @@ void move_tail_1(int player, int nx, int ny) {
 
 	if ((nx == 4 && ny == 1) || (nx == 5 && ny == 2) || (nx == 6 && ny == 2) ||
 		(nx == 7 && ny == 2) || (nx == 8 && ny == 2) || (nx == 9 && ny == 1)) {
-		// ÇÃ·¹ÀÌ¾î Å¬¸®¾îÇÑ °æ¿ì
+		
 		gotoxy(N_ROW + 2, 0);
 		printf("player%d clear", player);
 	}
@@ -124,7 +124,7 @@ void move_tail_1(int player, int nx, int ny) {
 }
 
 
-//¸ÞÀÎÇÔ¼ö
+
 void mugunghwa(void) {
 	sample_init_1();
 
@@ -159,19 +159,19 @@ void mugunghwa(void) {
 }
 
 
-//¹«±ÃÈ­²ÉÀÌÇÇ¾ú½À´Ï´Ù Ãâ·ÂÇÔ¼ö
+
 void print_mugung(void) {
 	gotoxy(N_ROW + 1, 0);
-	printf("¹« "); Sleep(350);
-	printf("±Ã "); Sleep(400);
-	printf("È­ "); Sleep(500);
-	printf("²É "); Sleep(550);
-	printf("ÀÌ "); Sleep(600);
-	printf("ÇÇ "); Sleep(450);
-	printf("¾ú "); Sleep(250);
-	printf("½À "); Sleep(200);
-	printf("´Ï "); Sleep(100);
-	printf("´Ù"); Sleep(3000);
+	printf("무 "); Sleep(350);
+	printf("궁 "); Sleep(400);
+	printf("화­ "); Sleep(500);
+	printf("꽃 "); Sleep(550);
+	printf("이 "); Sleep(600);
+	printf("피 "); Sleep(450);
+	printf("었 "); Sleep(250);
+	printf("습 "); Sleep(200);
+	printf("니 "); Sleep(100);
+	printf("다"); Sleep(3000);
 	gotoxy(N_ROW + 1, 0);
 	for (int i = 1; i < 30; i++) {
 		printf(" ");
